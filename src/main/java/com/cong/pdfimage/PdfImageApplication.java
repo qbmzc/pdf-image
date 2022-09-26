ce24f9152cb386a0b49e20c27535e2a34c7a472a
cong.zheng <congco@foxmail.com>
2022-09-26 22:26:13 +0800

    init

---
+++ src/main/java/com/cong/pdfimage/PdfImageApplication.java
@@ -0,0 +1,13 @@
+package com.cong.pdfimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfImageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfImageApplication.class, args);
+    }
+
+}
